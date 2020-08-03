f75a273b19a248e12a15fc37cc6bfc78172bd6e8
faelfer <rfapp00@gmail.com>
2020-08-03 15:43:58 -0300

    :art: improved code structure

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ import SignInScreen from './pages/SignInScreen';
 
 import AuthContext from './AuthContext';
 
-import { AuthReducer, initialState } from './reducers/authReducer'
+import { AuthReducer, initialState } from './reducers/authReducer';
 
 export default function Routes() {
     const [state, dispatch] = React.useReducer(
@@ -22,34 +22,34 @@ export default function Routes() {
     
     React.useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-        let userToken;
+        const bootstrapAsync = async () => {
+            let userToken;
 
-        try {
-            userToken = await AsyncStorage.getItem('userToken');
-        } catch (e) {
-        // Restoring token failed
-        }
+            try {
+                userToken = await AsyncStorage.getItem('userToken');
+            } catch (error) {
+            // Restoring token failed
+                console.log("Routes | useEffect | error: ", error);
+            }
 
-        // After restoring token, we may need to validate it in production apps
+            // After restoring token, we may need to validate it in production apps
 
-        // This will switch to the App screen or Auth screen and this loading
-        // screen will be unmounted and thrown away.
-        dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-    };
+            // This will switch to the App screen or Auth screen and this loading
+            // screen will be unmounted and thrown away.
+            dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+        };
 
-    bootstrapAsync();
+        bootstrapAsync();
     }, []);
     
-    const authContext = React.useMemo(
-    () => ({
+    const authContext = React.useMemo(() => ({
         signIn: async data => {
         // In a production app, we need to send some data (usually username, password) to server and get a token
         // We will also need to handle errors if sign in failed
         // After getting token, we need to persist the token using `AsyncStorage`
         // In the example, we'll use a dummy token
 
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
         },
         signOut: () => dispatch({ type: 'SIGN_OUT' }),
         signUp: async data => {
@@ -58,35 +58,33 @@ export default function Routes() {
         // After getting token, we need to persist the token using `AsyncStorage`
         // In the example, we'll use a dummy token
 
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
         },
-    }),
-    []
-    );
+    }),[]);
 
     return (
         <AuthContext.Provider value={authContext}>
             <NavigationContainer>
-            <Stack.Navigator>
-                {state.isLoading ? (
-                // We haven't finished checking for the token yet
-                <Stack.Screen name="Splash" component={SplashScreen} />
-                ) : state.userToken == null ? (
-                // No token found, user isn't signed in
-                <Stack.Screen
-                    name="SignIn"
-                    component={SignInScreen}
-                    options={{
-                    title: 'Sign in',
-                // When logging out, a pop animation feels intuitive
-                    animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-                    }}
-                />
-                ) : (
-                // User is signed in
-                <Stack.Screen name="Home" component={HomeScreen} />
-                )}
-            </Stack.Navigator>
+                <Stack.Navigator>
+                    {state.isLoading ? (
+                    // We haven't finished checking for the token yet
+                        <Stack.Screen name="Splash" component={SplashScreen} />
+                    ) : state.userToken == null ? (
+                    // No token found, user isn't signed in
+                        <Stack.Screen
+                            name="SignIn"
+                            component={SignInScreen}
+                            options={{
+                                title: 'Sign in',
+                                // When logging out, a pop animation feels intuitive
+                                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                            }}
+                        />
+                    ) : (
+                    // User is signed in
+                        <Stack.Screen name="Home" component={HomeScreen} />
+                    )}
+                </Stack.Navigator>
             </NavigationContainer>
         </AuthContext.Provider>
     );
