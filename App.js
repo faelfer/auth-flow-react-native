6ecab4040444c76393e7f162f0bc8f3b4ce4069c
faelfer <rfapp00@gmail.com>
2020-08-03 15:13:00 -0300

    :tada: initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import Routes from './src/routes';
+
+const App = () => {
+  return <Routes/>;
+};
+
+export default App;
